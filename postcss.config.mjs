825c5e5d6fafe3192caf85d34bdd6b2e5ef15081
Bby Lion <codelab042@gmail.com>
2025-11-10 04:19:04 +0100

    error fixing

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
-module.exports = {
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
   plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss,
+    autoprefixer,
   },
 };
