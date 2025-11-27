194b26c50f822c111c0a08591c99dc1163fd8a33
Bby Lion <codelab042@gmail.com>
2025-11-27 11:58:49 +0100

    not working

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+
+//   experimental: {
+//     optimizePackageImports: ["lucide-react"],
+//   },
+
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
