bc6515c7b30290ee237180252188c56449eb3fe5
Bby Lion <codelab042@gmail.com>
2025-11-27 13:33:36 +0100

    not working

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     turbo: false,        // Fully disables Turbopack
+//   },
+//   webpack: (config) => {
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false,        // ❗ Fully disables Turbopack
-  },
   webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      'why-is-node-running': false,
+      fs: false,
+      module: false,
+    };
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
