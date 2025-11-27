6dfd0d6dfa16e1fc768f8808f00708ed166c3ffd
Bby Lion <codelab042@gmail.com>
2025-11-27 14:20:03 +0100

    not working

--- next.config.js
+++ next.config.js
@@ -1,27 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     turbo: false,        // Fully disables Turbopack
-//   },
-//   webpack: (config) => {
-//     return config;
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  turbo: false, // <- disable Turbopack, use Webpack
-
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      'why-is-node-running': false,
-      fs: false,
-      module: false,
+      "pino-pretty": false,
+      "fs": false,
+      "net": false,
+      "tls": false,
     };
     return config;
   },
